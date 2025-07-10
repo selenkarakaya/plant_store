4ca96f2490dcec1f2446d5c6d79f4fcb5cecec64
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-07-10 07:26:13 +0100

    Redux store configuration

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,4 +1,6 @@
 // src/app/store.js
+// First, we will create the Redux store so that we can manage the global state in the application.
+
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../features/products/productSlice";
 
