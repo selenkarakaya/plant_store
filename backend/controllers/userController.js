51c1b941300f31629874d46ed150ee234b809fc2
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-07-21 20:17:36 +0100

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -81,7 +81,7 @@ const loginUser = asyncHandler(async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // true in prod, false in local
-    sameSite: "lax", // none in prod
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 1000 * 60 * 60 * 24, // 1 gün
   });
 
